7cc7f74e4067c1fedd7b3ac2ca372cce2e694903
Spandana Batchu <spandana.batchu@mutualmobile.com>
2018-02-09 15:15:31 +0530

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Spandana Batchu on 2/9/18.
+//  Copyright © 2018 MutualMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
