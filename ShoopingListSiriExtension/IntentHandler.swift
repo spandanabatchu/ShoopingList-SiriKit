3ab0c575185acf82400abbae9aa4683ba4932a77
Spandana Batchu <spandana.batchu@mutualmobile.com>
2018-02-20 16:57:39 +0530

    This is it!

--- ShoopingListSiriExtension/IntentHandler.swift
+++ ShoopingListSiriExtension/IntentHandler.swift
@@ -131,6 +131,7 @@ extension IntentHandler : INSetTaskAttributeIntentHandling {
                 let itemName = targetTask.title.spokenPhrase
                 activity.userInfo = [NSString(string: "item"):NSString(string: itemName)]
                 completion(INSetTaskAttributeIntentResponse(code: .failure, userActivity: activity))
+                print("Something to remove, later.")
             }
         }
     }
